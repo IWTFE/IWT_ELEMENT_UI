43626a08a8e98b8dcba1215473561f309cabdb47
suxiaoyan <105807174@qq.com>
2016-12-05 15:39:06 +0800

    修改route

--- examples/route.config.js
+++ examples/route.config.js
@@ -3,104 +3,104 @@ import langs from './i18n/route.json';
 
 
 const registerRoute = (navConfig) => {
-  let route = [];
-  Object.keys(navConfig).forEach((lang, index) => {
-    let navs = navConfig[lang];
+    let route = [];
+    Object.keys(navConfig).forEach((lang, index) => {
+        let navs = navConfig[lang];
     route.push({
-      path: `/${ lang }/component`,
-      redirect: `/${ lang }/component/installation`,
-      component: require(`./pages/${ lang }/component.vue`),
-      children: []
+        path: `/${ lang }/component`,
+        redirect: `/${ lang }/component/installation`,
+        component: require(`./pages/${ lang }/component.vue`),
+        children: []
     });
     navs.forEach(nav => {
-      if (nav.groups) {
+        if (nav.groups) {
         nav.groups.forEach(group => {
-          group.list.forEach(nav => {
+            group.list.forEach(nav => {
             addRoute(nav, lang, index);
-          });
-        });
-      } else if (nav.children) {
-        nav.children.forEach(nav => {
-          addRoute(nav, lang, index);
-        });
-      } else {
-        addRoute(nav, lang, index);
-      }
     });
-  });
-  function addRoute(page, lang, index) {
-    const component = page.path === '/changelog'
-      ? require(`./pages/${ lang }/changelog.vue`)
-      : require(`./docs/${ lang }${page.path}.md`);
-    let child = {
-      path: page.path.slice(1),
-      meta: {
-        title: page.title || page.name,
-        description: page.description,
-        lang
-      },
-      name: 'component-' + (page.title || page.name),
-      component: component.default || component
-    };
+    });
+    } else if (nav.children) {
+        nav.children.forEach(nav => {
+            addRoute(nav, lang, index);
+    });
+    } else {
+        addRoute(nav, lang, index);
+    }
+});
+});
+    function addRoute(page, lang, index) {
+        const component = page.path === '/changelog'
+            ? require(`./pages/${ lang }/changelog.vue`)
+            : require(`./docs/${ lang }${page.path}.md`);
+        let child = {
+            path: page.path.slice(1),
+            meta: {
+                title: page.title || page.name,
+                description: page.description,
+                lang
+            },
+            name: 'component-' + (page.title || page.name),
+            component: component.default || component
+        };
 
-    route[index].children.push(child);
-  }
+        route[index].children.push(child);
+    }
 
-  return route;
+    return route;
 };
 
 let route = registerRoute(navConfig);
 
 const generateMiscRoutes = function(lang) {
-  let guideRoute = {
-    path: `/${ lang }/guide`, // 指南
-    redirect: `/${ lang }/guide/design`,
-    component: require(`./pages/${ lang }/guide.vue`),
-    children: [{
-      path: 'design', // 设计原则
-      name: 'guide-design' + lang,
-      meta: { lang },
-      component: require(`./pages/${ lang }/design.vue`)
-    }, {
-      path: 'nav', // 导航
-      name: 'guide-nav' + lang,
-      meta: { lang },
-      component: require(`./pages/${ lang }/nav.vue`)
-    }]
-  };
+    let guideRoute = {
+        path: `/${ lang }/guide`, // 指南
+        redirect: `/${ lang }/guide/design`,
+        component: require(`./pages/${ lang }/guide.vue`),
+        children: [{
+            path: 'design', // 设计原则
+            name: 'guide-design' + lang,
+            meta: { lang },
+            component: require(`./pages/${ lang }/design.vue`)
+        }, {
+            path: 'nav', // 导航
+            name: 'guide-nav' + lang,
+            meta: { lang },
+            component: require(`./pages/${ lang }/nav.vue`)
+        }]
+    };
 
-  let resourceRoute = {
-    path: `/${ lang }/resource`, // 资源
-    meta: { lang },
-    name: 'resource' + lang,
-    component: require(`./pages/${ lang }/resource.vue`)
-  };
+    let resourceRoute = {
+        path: `/${ lang }/resource`, // 资源
+        meta: { lang },
+        name: 'resource' + lang,
+        component: require(`./pages/${ lang }/resource.vue`)
+    };
 
-  let mobileRoute = {
-    path: `/${ lang }/mobile`, // 移动端
-    meta: { lang },
-    name: 'mobile' + lang,
-    component: require(`./pages/${ lang }/mobile.vue`)
-  };
+    let mobileRoute = {
+        path: `/${ lang }/mobile`, // 移动端
+        meta: { lang },
+        name: 'mobile' + lang,
+        component: require(`./pages/${ lang }/mobile.vue`)
+    };
 
-  let indexRoute = {
-    path: `/${ lang }`, // 首页
-    meta: { lang },
-    name: 'home' + lang,
-    component: require(`./pages/${ lang }/index.vue`)
-  };
+    let indexRoute = {
+        path: `/${ lang }`, // 首页
+        meta: { lang },
+        name: 'home' + lang,
+        component: require(`./pages/${ lang }/index.vue`)
+    };
 
-  return [guideRoute, resourceRoute, indexRoute, mobileRoute];
+    return [guideRoute, resourceRoute, indexRoute, mobileRoute];
 };
 
 langs.forEach(lang => {
-  route = route.concat(generateMiscRoutes(lang.lang));
+    route = route.concat(generateMiscRoutes(lang.lang));
 });
 
 route.push({
-  path: '/play',
-  name: 'play',
-  component: require('./play/index.vue')
+    path: '/play',
+    name: 'play',
+    component: require('./play/index.vue')
 });
 
 
@@ -108,15 +108,15 @@ route.push({
 let userLanguage = localStorage.getItem('ELEMENT_LANGUAGE') || window.navigator.language || 'en-US';
 let defaultPath = '/en-US';
 if (userLanguage.indexOf('zh-') !== -1) {
-  defaultPath = '/zh-CN';
+    defaultPath = '/zh-CN';
 }
 
 route = route.concat([{
-  path: '/',
-  redirect: defaultPath
+    path: '/',
+    redirect: defaultPath
 }, {
-  path: '*',
-  redirect: defaultPath
+    path: '*',
+    redirect: defaultPath
 }]);
 
 export default route;
