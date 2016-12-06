f33f480ea1ef24f0ba25cbf7b7a3822664591886
dsq3033 <303306984@qq.com>
2016-12-06 10:34:54 +0800

    list

--- packages/print/src/main.vue
+++ packages/print/src/main.vue
@@ -1,18 +1,57 @@
 <template>
-  <div class="el-print">
-    <div class="el-print__header" v-if="$slots.header || header">
-      <slot name="header">{{ header }}</slot>
-    </div>
-    <div class="el-print__body" :style="bodyStyle">
-      <slot></slot>
-    </div>
-  </div>
+  <el-button v-on:click="printer()">打印</el-button>
 </template>
-
-<script>
+<script type="text/javascript">
+  import "./style.css";
   export default {
-    name: 'el-print',
-
-    props: ['header', 'bodyStyle']
-  };
+    name:'el-print',
+    props:{
+      target:{
+        type: String,
+        default: ''
+      },
+      'stylesheet' : {
+        type:String,
+        default:''
+      },
+      defaultStyle : {
+        type:String,
+        default : 'body { font-size: 14px; width:650px; margin:0 auto; } .title { text-align:center; } .border { border-collapse:collapse;border:1px solid #000; } .border td,.border th { border:1px solid #000; } .text-r { text-align:right; }'
+      }
+    },
+    methods:{
+      printer(){
+        if(this.target){
+          var target = document.getElementById(this.target);
+          if(!target) return false;
+          var table = target.cloneNode(true),
+              stylesheet = this["stylesheet"],
+              docfrag = document.createDocumentFragment(),
+              iframe = document.createElement("iframe"),
+              contWind = null;
+          if(stylesheet){
+            var style = document.createElement("link");
+            style.type = "text/css";
+            style.rel = "stylesheet";
+            style.href = stylesheet;
+          }else{
+              var style = document.createElement("style");
+              style.innerHTML = this.defaultStyle;
+          }
+          iframe.name = "frame";
+          iframe.style.display = "none";
+          document.body.appendChild(iframe);
+          var o = window.open("about:blank","frame");
+          docfrag.appendChild(table);
+          docfrag.appendChild(style);
+          contWind = iframe.contentWindow;
+          contWind.document.body.appendChild(docfrag);
+          contWind.print();
+          document.body.removeChild(iframe);
+        }else{
+          window.print();
+        }
+      }
+    }
+  }
 </script>
